51db7cc38ac51eebe642c92d71df21e0b651b613
Brendon Brinkmann <brendonbrinkmann@mac-mini.attlocal.net>
2022-04-06 16:14:08 -0500

    Update Sort and Search

--- squareproject/ViewController.swift
+++ squareproject/ViewController.swift
@@ -15,7 +15,7 @@ class ViewController: UIViewController {
     
     var directory: Directory? = nil
     var searchDirectory: Directory? = nil
-    
+    var sortType: Field = .noneSpecified
     var imageCache: [Int:UIImage?] = [:]
     
     @IBOutlet var tableView: UITableView!
@@ -112,6 +112,7 @@ extension ViewController {
     }
     
     public func sort(_ directory: Directory, by field: Field = .noneSpecified) -> Directory {
+        self.sortType = field
         var employees: [Employee]
         
         switch field {
@@ -177,8 +178,20 @@ extension ViewController: UITableViewDelegate, UITableViewDataSource {
 extension ViewController {
     func alert(sort: Bool = false, employeeCard: Int? = nil, errorType: UrlType = .malformed) {
         guard !sort else {
-            let sortAlert = UIAlertController(title: "Sort", message: "", preferredStyle: .alert)
-            sortAlert.addAction(UIAlertAction(title: "Default", style: .default, handler: { _ in
+            
+            var message: String = ""
+            switch self.sortType {
+            case .noneSpecified: message = "Default"
+            case .team: message = "Team"
+            case .firstName: message = "First Team"
+            case .lastName: message = "Last Name"
+            case .employeeType: message = "Employee Type"
+            case .phoneNumber: message = "Phone Number"
+            case .uuid, .biography, .email: break
+            }
+            
+            let sortAlert = UIAlertController(title: "Sort", message: message, preferredStyle: .alert)
+            sortAlert.addAction(UIAlertAction(title: "Default", style: .destructive, handler: { _ in
                 if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .noneSpecified) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
@@ -188,9 +201,8 @@ extension ViewController {
                 self.tableView.isHidden = false
                 self.tableView.reloadData()
             }))
-
             sortAlert.addAction(UIAlertAction(title: "Team", style: .default, handler: { _ in
-                if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .team) }
+                if let directory = self.directory { self.directory = self.sort(directory, by: .team) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
                     self.tableView.isHidden = true
@@ -200,7 +212,7 @@ extension ViewController {
                 self.tableView.reloadData()
             }))
             sortAlert.addAction(UIAlertAction(title: "First Name", style: .default, handler: { _ in
-                if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .firstName) }
+                if let directory = self.directory { self.directory = self.sort(directory, by: .firstName) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
                     self.tableView.isHidden = true
@@ -210,7 +222,7 @@ extension ViewController {
                 self.tableView.reloadData()
             }))
             sortAlert.addAction(UIAlertAction(title: "Last Name", style: .default, handler: { _ in
-                if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .lastName) }
+                if let directory = self.directory { self.directory = self.sort(directory, by: .lastName) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
                     self.tableView.isHidden = true
@@ -220,7 +232,7 @@ extension ViewController {
                 self.tableView.reloadData()
             }))
             sortAlert.addAction(UIAlertAction(title: "Employee Type", style: .default, handler: { _ in
-                if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .employeeType) }
+                if let directory = self.directory { self.directory = self.sort(directory, by: .employeeType) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
                     self.tableView.isHidden = true
@@ -230,7 +242,7 @@ extension ViewController {
                 self.tableView.reloadData()
             }))
             sortAlert.addAction(UIAlertAction(title: "Phone Number", style: .default, handler: { _ in
-                if let directory = self.searchDirectory { self.directory = self.sort(directory, by: .phoneNumber) }
+                if let directory = self.directory { self.directory = self.sort(directory, by: .phoneNumber) }
                 guard !(self.directory?.isEmpty() ?? false) else {
                     self.noResultsLabel.isHidden = false
                     self.tableView.isHidden = true
@@ -240,7 +252,7 @@ extension ViewController {
                 self.tableView.reloadData()
             }))
             
-            sortAlert.addAction(UIAlertAction(title: "Dismiss", style: .destructive, handler: nil))
+            sortAlert.addAction(UIAlertAction(title: "Dismiss", style: .cancel, handler: nil))
 
             self.present(sortAlert, animated: true, completion: nil)
             return
@@ -268,6 +280,7 @@ extension ViewController {
 
 extension ViewController {
     @objc private func refreshDirectory(_ sender: Any) {
+        self.sortType = .noneSpecified
         self.fetchData(with: .directory)
     }
 }
@@ -276,7 +289,9 @@ extension ViewController {
 
 extension ViewController: UISearchBarDelegate {
     func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-        if let directory = self.searchDirectory { self.directory = self.search(directory, by: searchText) }
+        if let directory = self.searchDirectory {
+            self.directory = self.sort(self.search(directory, by: searchText), by: self.sortType)
+        }
         guard !(self.directory?.isEmpty() ?? false) else {
             self.noResultsLabel.isHidden = false
             self.tableView.isHidden = true
@@ -284,6 +299,10 @@ extension ViewController: UISearchBarDelegate {
         }
         self.tableView.isHidden = false
         self.tableView.reloadData()
+    }
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        searchBar.resignFirstResponder()
     }
     
     func setupKeyboard() {
@@ -309,18 +328,4 @@ extension ViewController {
     }
 }
 
-// Temp
-//employees: [
-//    Employee(
-//        uuid: "0d8fcc12-4d0c-425c-8355-390b312b909c",
-//        full_name: "Justine Mason",
-//        phone_number: "5553280123",
-//        email_address: "jmason.demo@squareup.com",
-//        biography: "Engineer on the Point of Sale team.",
-//        photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/16c00560-6dd3-4af4-97a6-d4754e7f2394/small.jpg",
-//        photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/16c00560-6dd3-4af4-97a6-d4754e7f2394/large.jpg",
-//        team: "Point of Sale", employee_type: "FULL_TIME"
-//    )
-//]
-//search [squareproject.Directory.Employee(uuid: "a98f8a2e-c975-4ba3-8b35-01f719e7de2d", full_name: "Camille Rogers", phone_number: "5558531970", email_address: "crogers.demo@squareup.com", biography: "Designer on the web marketing team.", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/5095a907-abc9-4734-8d1e-0eeb2506bfa8/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/5095a907-abc9-4734-8d1e-0eeb2506bfa8/large.jpg", team: "Public Web & Marketing", employee_type: "PART_TIME"), squareproject.Directory.Employee(uuid: "b8cf3382-ecf2-4240-b8ab-007688426e8c", full_name: "Richard Stein", phone_number: "5557223332", email_address: "rstein.demo@squareup.com", biography: "Product manager for the Point of sale app!", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/43ed39b3-fbc0-4eb8-8ed3-6a8de479a52a/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/43ed39b3-fbc0-4eb8-8ed3-6a8de479a52a/large.jpg", team: "Point of Sale", employee_type: "PART_TIME"), squareproject.Directory.Employee(uuid: "61b21d34-5499-401a-98b3-16f26e645d54", full_name: "Alaina Daly", phone_number: "5555442937", email_address: "adaly.demo@squareup.com", biography: "Product marketing manager for the Retail Point of Sale app in New York.", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/57bc7ed2-5f9e-4814-a7df-dea85c2ed97f/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/57bc7ed2-5f9e-4814-a7df-dea85c2ed97f/large.jpg", team: "Retail", employee_type: "FULL_TIME"), squareproject.Directory.Employee(uuid: "b6dea526-c571-4d43-8b41-375ca5cd9fdb", full_name: "Elisa Rizzo", phone_number: "5552234497", email_address: "erizzo.demo@squareup.com", biography: "iOS Engineer on the Restaurants team.", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/8ab10188-74d0-4843-9eb2-1938571f6830/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/8ab10188-74d0-4843-9eb2-1938571f6830/large.jpg", team: "Restaurants", employee_type: "FULL_TIME"), squareproject.Directory.Employee(uuid: "8623ba77-9d6a-4bcd-bd91-e19ae2c9dba2", full_name: "Ryan Gehani", phone_number: "5554047710", email_address: "rgehani.demo@squareup.com", biography: "Product manager for Invoices!", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/7959987e-0d64-4bf6-8b9e-da78deac3457/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/7959987e-0d64-4bf6-8b9e-da78deac3457/large.jpg", team: "Invoices", employee_type: "FULL_TIME"), squareproject.Directory.Employee(uuid: "7fb13023-d013-41ac-84f1-e554890ccb32", full_name: "Tim Nakamura", phone_number: "5557510409", email_address: "tnakamura.demo@squareup.com", biography: "Hardware packaging designer on the hardware team, working from LA.", photo_url_small: "https://s3.amazonaws.com/sq-mobile-interview/photos/e2b088e6-0b8d-4295-a66c-d7181cdec3d6/small.jpg", photo_url_large: "https://s3.amazonaws.com/sq-mobile-interview/photos/e2b088e6-0b8d-4295-a66c-d7181cdec3d6/large.jpg", team: "Hardware", employee_type: "CONTRACTOR")] by: R
 typealias CompletionHandler = (_ image: UIImage) -> Void
